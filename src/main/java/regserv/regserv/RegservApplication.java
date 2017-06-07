841e69d407b31ec54bcc871e07856e581662248e
Kirk Lortz <kirk.lortz@cgi.com>
2017-06-07 12:08:28 -0400

    Initializing Repository

---
+++ src/main/java/regserv/regserv/RegservApplication.java
@@ -0,0 +1,12 @@
+package regserv.regserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegservApplication.class, args);
+	}
+}
